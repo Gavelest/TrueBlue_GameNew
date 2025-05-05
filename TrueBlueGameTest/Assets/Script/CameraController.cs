a90860622ebef18294ccd40270f0e09e195a04aa
Lani <leilanimumford@outlook.com>
2025-05-05 17:06:54 +0800

    Importing FBX Files and New Camera
    Imported some more FBX files, including a new enemy and world props. Made a new scene dedicated to altering those assets under 'MasterAsset'. Also made a new scene with a long walkway to test and implement a new camera control and will add in some assets for upcoming testing.

---
+++ TrueBlueGameTest/Assets/Script/CameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform player;
+    public float moveSpeed;
+    public Vector3 offset;
+    public float followDistance;
+    public Quaternion rotation;
+    
+    private void Update() {
+        Vector3 pos = Vector3.Lerp(transform.position, player.position + offset + -transform.forward * followDistance, moveSpeed * Time.deltaTime);
+        transform.position = pos;
+
+        transform.rotation = rotation;
+    }
+
+}
+
