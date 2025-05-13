fc1a10bf621149e139276e02edb4aa00693c0bfb
Gavelest <connorgavin2020@gmail.com>
2025-05-13 20:56:47 +0800

    multiple scripts for the battle system
    battle system tracks and makes the enemies attack

---
+++ TrueBlueGameTest/Assets/HandleTurn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class HandleTurn 
+{
+
+    public string Attacker;
+    public string Type;
+    public GameObject AttackersGameObject;
+    public GameObject AttackersTarget;
+
+
+
+}
--- TrueBlueGameTest/Assets/HeroStateMachine.cs
+++ TrueBlueGameTest/Assets/HeroStateMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroStateMachine : MonoBehaviour
 {
@@ -21,10 +22,16 @@ public class HeroStateMachine : MonoBehaviour
 
     public TurnState currentState;
 
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+    public Image ProgressBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        currentState = TurnState.PROCESSING;
+
     }
 
     // Update is called once per frame
@@ -35,6 +42,7 @@ public class HeroStateMachine : MonoBehaviour
         {
 
             case (TurnState.PROCESSING):
+                UpgradeProgressBar();
             break;
 
             case (TurnState.ADDTOLIST):
@@ -64,7 +72,15 @@ public class HeroStateMachine : MonoBehaviour
     void UpgradeProgressBar()
     {
 
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        float calc_cooldown = cur_cooldown / max_cooldown;
+        ProgressBar.transform.localScale = new Vector3(Mathf.Clamp(calc_cooldown, 0, 1), ProgressBar.transform.localScale.y, ProgressBar.transform.localScale.z);
+        if(cur_cooldown >= max_cooldown)
+        {
 
+            currentState = TurnState.ADDTOLIST;
+
+        }
 
     }
 
