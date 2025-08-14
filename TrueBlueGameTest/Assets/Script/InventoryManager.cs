88c889b31a207a20fd71514f455d88d474c7e3f1
GlitzyBunny <GlitzyBunny21@gmail.com>
2025-08-14 20:26:39 +0800

    Start of inventory
    Very baby beginning of inventory!
    If you hit 'e' it should open a bunch of boxes im gonna edit and stuff. Items and such are in the works I just wanna eat dinner im hungry.

---
+++ TrueBlueGameTest/Assets/Script/InventoryManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    //This manages the inventory system in the game! Right now the button for it is set to 'e' in the unity project settings
+    public GameObject InventoryMenu;
+    private bool menuActivated; //checks if on or off
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && menuActivated) //activates and deactivates menu when hitting the inventory button
+        {
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+
+        else if (Input.GetButtonDown("Inventory") && !menuActivated) //I spent 20 minutes confused one why this wasnt working and I had forgotten to put the '!' i actually want to feed a baby highly corrosive liquids
+        {
+            Time.timeScale = 0; //might cause issues with animations if you want to add them I say as im commenting to myself
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+}
