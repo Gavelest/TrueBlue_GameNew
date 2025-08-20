740cba8412ed83ce36d1a7743c985a62e94100d1
Gavelest <connorgavin2020@gmail.com>
2025-08-20 21:29:20 +0800

    Movement fixed by Mel

--- TrueBlueGameTest/Assets/Script/PlayerMovement/PlayerMovement.cs
+++ TrueBlueGameTest/Assets/Script/PlayerMovement/PlayerMovement.cs
@@ -44,14 +44,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void RotateFromMouseVector()
     {
-        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+        //capture the mouse position in screenspace
+        //then scale the z offset to align it with the player's position - note that Camera.main is different from 'Camera'
+        var screenspaceMousePosition = Input.mousePosition;
+        screenspaceMousePosition.z = Vector3.Distance(Camera.main.transform.position, transform.position);
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
-        {
-            var target = hitInfo.point;
-            target.y = transform.position.y;
-            transform.LookAt(target);
-        }
+        //transform the mouse position into world space so that we can compare it to the player
+        var worldspaceMousePosition = Camera.main.ScreenToWorldPoint(screenspaceMousePosition);
+
+        //calculate the look direction as an offset from the player's position
+        var lookDirection = worldspaceMousePosition - transform.position;
+        lookDirection.y = 0; //clear anything on the y axis
+        lookDirection.Normalize(); //convert to unit vector
+
+        //Debug.DrawRay(transform.position, lookDirection);
+        //apply to player
+        transform.forward = lookDirection;
     }
 
     private Vector3 MoveTowardTarget(Vector3 targetVector)
