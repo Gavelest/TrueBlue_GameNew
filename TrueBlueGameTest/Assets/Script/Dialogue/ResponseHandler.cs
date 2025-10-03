c24e86298b0955f552fd6e247111ecaa2b0f08fe
Lani <leilanimumford@outlook.com>
2025-10-03 19:33:18 +0800

    Dialogue WIP #2
    it works!! moving onto interactions next time

--- TrueBlueGameTest/Assets/Script/Dialogue/ResponseHandler.cs
+++ TrueBlueGameTest/Assets/Script/Dialogue/ResponseHandler.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ResponseHandler : MonoBehaviour
 {
     [SerializeField] private RectTransform responseBox;
     [SerializeField] private RectTransform responseButtonTemplate;
     [SerializeField] private RectTransform responseContainer;
+
+    private DialogueUI dialogueUI;
+
+    private List<GameObject> tempResponseButtons = new List<GameObject>();
+
+    private void Start()
+    {
+        dialogueUI=GetComponent<DialogueUI>();
+    }
 
     public void ShowResponses(Response[] responses)
     {
@@ -19,6 +29,8 @@ public class ResponseHandler : MonoBehaviour
             responseButton.GetComponent<TMP_Text>().text = response.ResponseText;
             responseButton.GetComponent<Button>().onClick.AddListener(() => OnPickedResponse(response));
 
+            tempResponseButtons.Add(responseButton);
+
             responseBoxHeight += responseButtonTemplate.sizeDelta.y;
         }
 
@@ -28,6 +40,14 @@ public class ResponseHandler : MonoBehaviour
 
     private void OnPickedResponse(Response response)
     {
+        responseBox.gameObject.SetActive(false);
 
+        foreach (GameObject button in tempResponseButtons)
+        {
+            Destroy(button);
+        }
+        tempResponseButtons.Clear();
+
+        dialogueUI.ShowDialogue(response.DialogueObject);
     }
 }
